ab1d4ddf0bebf5c2d96008a266356a7511f2d4c5
LeTuanVu-Dev <letuanvu425@gmail.com>
2023-09-17 19:55:17 +0700

    validate

--- app/src/main/java/com/mobiai/base/basecode/extensions/Views.kt
+++ app/src/main/java/com/mobiai/base/basecode/extensions/Views.kt
@@ -1,5 +1,6 @@
 package com.base.lqlbasecode.extensions
 
+import android.view.View
 import android.widget.ScrollView
 
 fun ScrollView.scrollToBottom() {
@@ -7,4 +8,13 @@ fun ScrollView.scrollToBottom() {
     val bottom = lastChild.bottom + paddingBottom
     val delta = bottom - (scrollY+ height)
     smoothScrollBy(0, delta)
+}
+fun View.visible(){
+    this.visibility = View.VISIBLE
+}
+fun View.invisible(){
+    this.visibility = View.INVISIBLE
+}
+fun View.gone(){
+    this.visibility = View.GONE
 }
