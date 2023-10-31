6d7ec6986db19945c62c034fc2b9de713d31d7f4
LeTuanVu-Dev <letuanvu425@gmail.com>
2023-10-31 15:14:28 +0700

    Lấy danh sách lesson

--- app/src/main/java/com/mobiai/app/ui/fragment/LessonFragment.kt
+++ app/src/main/java/com/mobiai/app/ui/fragment/LessonFragment.kt
@@ -5,13 +5,17 @@ import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.gson.Gson
+import com.jaygoo.widget.RangeSeekBar
 import com.mobiai.app.App
+import com.mobiai.app.model.Lessons
 import com.mobiai.app.model.Question
+import com.mobiai.app.model.Results
 import com.mobiai.app.utils.makeGone
 import com.mobiai.app.utils.makeVisible
 import com.mobiai.app.utils.setOnSafeClickListener
@@ -40,11 +44,18 @@ class LessonFragment : BaseFragment<FragmentLessonBinding>() {
 
     private var numberTopic:String? = null
     private var listDataWithTopic = arrayListOf<Question>()
-    val level1List = mutableListOf<Question>()
-    val level2List = mutableListOf<Question>()
-    val level3List = mutableListOf<Question>()
-    val level4List = mutableListOf<Question>()
-    val level5List = mutableListOf<Question>()
+    private var listDataLessonWithTopic = arrayListOf<Lessons>()
+    private val level1List = mutableListOf<Question>()
+    private val level2List = mutableListOf<Question>()
+    private val level3List = mutableListOf<Question>()
+    private val level4List = mutableListOf<Question>()
+    private val level5List = mutableListOf<Question>()
+
+    private val LEVEL_1 = 1
+    private val LEVEL_2 = 2
+    private val LEVEL_3 = 3
+    private val LEVEL_4 = 4
+    private val LEVEL_5 = 5
     override fun initView() {
         arguments.let {
             if (it != null) {
@@ -52,6 +63,9 @@ class LessonFragment : BaseFragment<FragmentLessonBinding>() {
                 Log.d("TAG", "initView: $numberTopic")
             }
         }
+
+        getDataLesson()
+
         getData(object : GetListDataFromFirebase<Question>{
             override fun getListDataSuccess(list: List<Question>) {
                 for(i in list){
@@ -75,8 +89,51 @@ class LessonFragment : BaseFragment<FragmentLessonBinding>() {
             handlerBackPressed()
         }
 
-        binding.ivStart.setOnClickListener {
-            addFragment(QuestionFragment.instance(getListRandomForLevel(1),"lesson1"))
+        binding.frStudy1.setOnSafeClickListener {
+            addFragmentWithLevel(LEVEL_1)
+        }
+
+        binding.frStudy2.setOnSafeClickListener {
+            addFragmentWithLevel(LEVEL_2)
+        }
+
+        binding.frStudy3.setOnSafeClickListener {
+            addFragmentWithLevel(LEVEL_3)
+        }
+
+        binding.frStudy4.setOnSafeClickListener {
+            addFragmentWithLevel(LEVEL_4)
+        }
+
+
+        binding.frStudy5.setOnSafeClickListener {
+            addFragmentWithLevel(LEVEL_5)
+        }
+
+        binding.frStudy6.setOnSafeClickListener {
+            addFragmentWithLevel(LEVEL_5)
+        }
+
+    }
+
+    private fun addFragmentWithLevel(level: Int){
+        for (item in listDataLessonWithTopic){
+            if (getNumberLevel(item.lessonCode) == level){
+                addFragment(QuestionFragment.instance(getListRandomForLevel(getNumberLevel(item.lessonCode)),item.lessonCode))
+                break
+            }
+        }
+    }
+    private fun getNumberLevel(inputString:String):Int {
+        // Sử dụng regex để lấy ra số từ chuỗi
+        val regex = Regex("\\d+")
+        val matchResult = regex.find(inputString)
+
+        // Kiểm tra xem có kết quả phù hợp nào không
+        if (matchResult != null) {
+            return matchResult.value.toInt()
+        } else {
+            return -1
         }
     }
 
@@ -84,6 +141,31 @@ class LessonFragment : BaseFragment<FragmentLessonBinding>() {
         super.handlerBackPressed()
         closeFragment(this)
     }
+
+    private fun getDataLesson(){
+        binding.frLoading.makeVisible()
+        val db = FirebaseDatabase.getInstance()
+        val ref1 = db.getReference(App.LESSON)
+        //lesson
+        ref1.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (userSnapshot in dataSnapshot.children) {
+                    val lesson = userSnapshot.getValue(Lessons::class.java)
+                    if (lesson != null) {
+                       if (lesson.topicCode ==  numberTopic){
+                           listDataLessonWithTopic.add(lesson)
+                       }
+                    }
+                }
+                Log.w("TAG", "listDataLesson: $listDataLessonWithTopic")
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("TAG", "Failed to read value.", error.toException())
+            }
+        })
+    }
+
 
 
     private fun getData(getListDataFromFirebase: GetListDataFromFirebase<Question>){
@@ -116,7 +198,7 @@ class LessonFragment : BaseFragment<FragmentLessonBinding>() {
         })
     }
 
-    fun getListRandomForLevel(level : Int): String{
+    private fun getListRandomForLevel(level : Int): String{
         val listQuestion : ArrayList<Question> = arrayListOf()
         val random = Random()
        var num1 = 0
